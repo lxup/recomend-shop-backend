ac323d301496ce8a470ad8a70db2b9158c740ae0
loup <loup.quehec@gmail.com>
2023-11-10 13:03:48 +0100

    fix coolify

--- medusa-config.js
+++ medusa-config.js
@@ -77,7 +77,11 @@ const projectConfig = {
   database_url: DATABASE_URL,
   admin_cors: ADMIN_CORS,
   // Uncomment the following lines to enable REDIS
-  redis_url: REDIS_URL
+  redis_url: REDIS_URL,
+  database_extra:
+      process.env.NODE_ENV !== 'development'
+        ? { ssl: { rejectUnauthorized: false } }
+        : {},
 };
 
 /** @type {import('@medusajs/medusa').ConfigModule} */
