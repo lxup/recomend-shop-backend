acfd0e5c8a677257ea712412b970c987f1aa28ef
loup <loup.quehec@gmail.com>
2023-11-11 11:13:59 +0100

    fix

--- medusa-config.js
+++ medusa-config.js
@@ -62,14 +62,29 @@ const plugins = [
   {
     resolve: `medusa-plugin-meilisearch`,
     options: {
-      // config object passed when creating an instance
-      // of the MeiliSearch client
-      config: {
-        host: process.env.MEILISEARCH_HOST,
-        apiKey: process.env.MEILISEARCH_API_KEY,
-      },
+      // other options...
       settings: {
-        // index settings...
+        products: {
+          indexSettings: {
+            searchableAttributes: [
+              "title", 
+              "description",
+              "variant_sku",
+            ],
+            displayedAttributes: [
+              "title", 
+              "description", 
+              "variant_sku", 
+              "thumbnail", 
+              "handle",
+            ],
+          },
+          primaryKey: "id",
+          transformer: (product) => ({
+            id: product.id, 
+            // other attributes...
+          }),
+        },
       },
     },
   },
