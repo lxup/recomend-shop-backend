982a4b151ff9586a0ceafd58b83d783e0460c7e1
loup <loup.quehec@gmail.com>
2023-11-29 00:10:15 +0100

    Update medusa-config.js

--- medusa-config.js
+++ medusa-config.js
@@ -110,8 +110,7 @@ const plugins = [
   {
     resolve: `medusa-payment-paypal`,
     options: {
-      // sandbox: process.env.PAYPAL_SANDBOX,
-      sandbox: undefined,
+      sandbox: process.env.PAYPAL_SANDBOX,
       clientId: process.env.PAYPAL_CLIENT_ID,
       clientSecret: process.env.PAYPAL_CLIENT_SECRET,
       authWebhookId: process.env.PAYPAL_AUTH_WEBHOOK_ID,
